95da6904459a4a1eb22efc06c047414ac4ae3e80
Jackie Li <jackie.li@target-energysolutions.com>
2023-04-06 14:01:02 +0100

    increase side panel resize handle

--- public/app/features/dashboard/dashgrid/SidePanel.tsx
+++ public/app/features/dashboard/dashgrid/SidePanel.tsx
@@ -118,7 +118,7 @@ function SidePanelResize({ width: pw, height: ph, onResizeStop, children }: Side
           className={cx(
             'react-resizable-handle',
             css`
-              width: 2px;
+              width: 5px;
               position: absolute;
               right: 0;
               top: 0;
